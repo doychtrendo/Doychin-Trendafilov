29f6cc16fc72137099206f851a758c4344934ba2
Doychin Trendafilov <doychin.trendafilov.a50@learn.telerikacademy.com>
2023-05-11 02:19:53 +0300

    Cause merge conflict 1

--- 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
+++ 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Git!");
-
-        System.out.println("Now I can continue make changes to my project");
+        System.out.println("Let's cause a merge conflict");
     }
 }
