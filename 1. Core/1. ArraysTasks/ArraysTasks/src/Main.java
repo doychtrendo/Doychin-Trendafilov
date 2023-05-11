d293f2757ab4965a68d99012ac21d184e69cb73f
Doychin Trendafilov <doychin.trendafilov.a50@learn.telerikacademy.com>
2023-05-11 13:02:38 +0300

    Pull changes from remote and merge them before pushing

--- 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
+++ 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Change the remote copy third time and use Stash."
+        System.out.println("Let's cause a merge conflict and resolve it with Stash.");
     }
 }
