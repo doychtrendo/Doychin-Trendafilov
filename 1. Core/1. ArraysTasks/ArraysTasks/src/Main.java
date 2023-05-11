1a871d6b1380be5a393f27267a33a054da6bb292
Doychin Trendafilov <doychin.trendafilov.a50@learn.telerikacademy.com>
2023-05-11 12:43:37 +0300

    Cause merge conflict 2

--- 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
+++ 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Let's cause a merge conflict");
-        System.out.println("Now I can modify the remote copy of the repository again.");
+        System.out.println("Now I can cause merge conflict with the remote copy again.");
     }
 }
