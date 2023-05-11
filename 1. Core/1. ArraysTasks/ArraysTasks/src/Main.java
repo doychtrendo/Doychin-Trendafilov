1d4e213aa24b35e310c20e22eca94cc11371cfd9
Doychin Trendafilov <82344155+doychtrendo@users.noreply.github.com>
2023-05-11 12:42:23 +0300

    Update Main.java

--- 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
+++ 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Let's cause a merge conflict");
+        System.out.println("Now I can modify the remote copy of the repository again.");
     }
 }
