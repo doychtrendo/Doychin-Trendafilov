8cf8c5e2b2c3c79af5af9bbfdb3cd5ffbd4f77d7
Doychin Trendafilov <82344155+doychtrendo@users.noreply.github.com>
2023-05-11 12:56:26 +0300

    Update Main.java third time and use Stash

--- 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
+++ 1. Core/1. ArraysTasks/ArraysTasks/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Let's cause a merge conflict");
-        System.out.println("Now I can cause merge conflict with the remote copy again.");
+        System.out.println("Change the remote copy third time and use Stash."
     }
 }
