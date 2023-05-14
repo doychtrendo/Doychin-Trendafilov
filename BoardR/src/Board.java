4908223ac847e6fc281ea6c77d7c74a78f25f244
Doychin Trendafilov <doychin.trendafilov.a50@learn.telerikacademy.com>
2023-05-14 18:09:22 +0300

    Update BoardR

--- BoardR/src/Board.java
+++ BoardR/src/Board.java
@@ -7,10 +7,12 @@ public class Board {
     //CONSTRUCTOR
     private Board() {}
 
+    //METHOD
     public static void addItem(BoardItem item) {
         items.add(item);
     }
 
+    //METHOD
     public static List<BoardItem> getItems() {
         return items;
     }
