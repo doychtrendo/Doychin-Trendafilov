46b8bd31aaace08230b70117298b42320774d629
Doychin <doych_trendo@yahoo.com>
2023-11-27 17:19:23 +0200

    create BingPage in SearchEngineAutomationTests

--- SearchEngineAutomationTests/src/main/java/org/example/pages/BingPage.java
+++ SearchEngineAutomationTests/src/main/java/org/example/pages/BingPage.java
@@ -1,4 +1,30 @@
 package org.example.pages;
 
-public class BingPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class BingPage extends BasePage {
+
+    public BingPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateToBing() {
+        driver.get("https://www.bing.com");
+    }
+
+    public void acceptConsent() {
+        WebElement acceptButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='bnp_btn_accept']")));
+        acceptButton.click();
+    }
+
+    public void search(String searchTerm) {
+        WebElement searchField = driver.findElement(By.xpath("//*[@id='sb_form_q']"));
+        searchField.sendKeys(searchTerm);
+
+        WebElement searchButton = driver.findElement(By.xpath("//label[@for='sb_form_go']"));
+        searchButton.click();
+    }
 }
