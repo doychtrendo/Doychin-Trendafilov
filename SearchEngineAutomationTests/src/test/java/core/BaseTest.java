d9663d796b29e7698423d7d00a2ba66d2257dccc
Doychin <doych_trendo@yahoo.com>
2023-11-27 17:42:26 +0200

    create BaseTest in SearchEngineAutomationTests

--- SearchEngineAutomationTests/src/test/java/core/BaseTest.java
+++ SearchEngineAutomationTests/src/test/java/core/BaseTest.java
@@ -1,4 +1,21 @@
 package core;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+
+    protected static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
 }
