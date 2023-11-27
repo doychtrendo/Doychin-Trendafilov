ad5c5a4cce4adc867e00471443825a2bfd13132e
Doychin <doych_trendo@yahoo.com>
2023-11-27 17:51:37 +0200

    refactor the BingSearchByTermTest given the new POM in SearchEngineAutomationTests

--- SearchEngineAutomationTests/src/test/java/bingsearchtest/BingSearchByTermTest.java
+++ SearchEngineAutomationTests/src/test/java/bingsearchtest/BingSearchByTermTest.java
@@ -1,68 +1,31 @@
 package bingsearchtest;
 
-import org.junit.jupiter.api.*;
+import core.BaseTest;
+import org.example.pages.BingPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class BingSearchByTermTest {
-
-    private static WebDriver driver;
-    private static WebDriverWait wait;
-
-    @BeforeAll
-    public static void classSetUp() {
-        //set up browser
-        driver = new ChromeDriver();
-
-        //navigate to bing.com
-        driver.get("https://www.bing.com");
-
-        //agree to consent
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement acceptButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='bnp_btn_accept']")));
-        acceptButton.click();
-    }
-
-    @AfterAll
-    public static void classTearDown() {
-        //close the browser
-        driver.close();
-    }
-
-    @BeforeEach
-    public void testSetUp() {
-        //navigate to bing.com
-        driver.get("https://www.bing.com");
-    }
+public class BingSearchByTermTest extends BaseTest {
 
     @Test
     public void resultFound_when_searchTermProvided() {
+        BingPage bingHomePage = new BingPage(driver);
+        bingHomePage.navigateToBing();
+        bingHomePage.acceptConsent();
+
         String searchTerm = "Telerik Academy Alpha";
         String expectedTitle = "IT Career Start in 6 Months - Telerik Academy Alpha";
 
-        //type text in search field
-        WebElement searchField = driver.findElement(By.xpath("//*[@id='sb_form_q']"));
-        searchField.sendKeys(searchTerm);
+        bingHomePage.search(searchTerm);
 
-        //click search button
-        WebElement searchButton = driver.findElement(By.xpath("//label[@for='sb_form_go']"));
-        searchButton.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[@class=' b_topTitle']")));
 
-        //wait for search results
-        WebElement firstResultTitleElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[@class=' b_topTitle']")));
-
-        //get the text for the first result
+        WebElement firstResultTitleElement = driver.findElement(By.xpath("//h2[@class=' b_topTitle']"));
         String firstResultTitle = firstResultTitleElement.getText();
 
-        //validate the title of the first result
         Assertions.assertEquals(expectedTitle, firstResultTitle, "Search result title does not match. Expected: " + expectedTitle + ", Actual: " + firstResultTitle);
-
     }
-
 }
--- SearchEngineAutomationTests/src/test/java/core/BaseTest.java
+++ SearchEngineAutomationTests/src/test/java/core/BaseTest.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BaseTest {
 
     protected static WebDriver driver;
+    protected static WebDriverWait wait;
 
     @BeforeAll
     public static void setUp() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterAll
