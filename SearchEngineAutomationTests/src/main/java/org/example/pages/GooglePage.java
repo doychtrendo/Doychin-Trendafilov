97bd2add53d6511a9aa099acd5e5c0a314a0a647
Doychin <doych_trendo@yahoo.com>
2023-11-27 17:25:28 +0200

    create GooglePage in SearchEngineAutomationTests

--- SearchEngineAutomationTests/src/main/java/org/example/pages/GooglePage.java
+++ SearchEngineAutomationTests/src/main/java/org/example/pages/GooglePage.java
@@ -1,4 +1,30 @@
 package org.example.pages;
 
-public class GooglePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class GooglePage extends BasePage {
+
+    public GooglePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateToGoogle() {
+        driver.get("https://www.google.com");
+    }
+
+    public void acceptConsent() {
+        WebElement acceptButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='L2AGLb']")));
+        acceptButton.click();
+    }
+
+    public void search(String searchTerm) {
+        WebElement searchField = driver.findElement(By.xpath("//textarea[@type='search']"));
+        searchField.sendKeys(searchTerm);
+
+        WebElement searchButton = driver.findElement(By.xpath("(//input[@type='submit' and @name='btnK'])[2]"));
+        searchButton.click();
+    }
 }
