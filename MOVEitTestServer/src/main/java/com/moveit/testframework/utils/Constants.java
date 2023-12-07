f073506df2e3c9144947f967c5cebb5dca091b18
Doychin <doych_trendo@yahoo.com>
2023-12-07 16:52:37 +0200

    Tidy up Constants class

--- MOVEitTestServer/src/main/java/com/moveit/testframework/utils/Constants.java
+++ MOVEitTestServer/src/main/java/com/moveit/testframework/utils/Constants.java
@@ -2,22 +2,26 @@ package com.moveit.testframework.utils;
 
 public class Constants {
 
+    // URL and Path Constants
     public static final String HOME_PAGE = "MOVEit.homePage";
     public static final String LOGIN_PAGE = "MOVEit.loginPage";
+    public static final String TESTDATA_PATH = "src/test/resources/testdata/";
+
+    // User Credentials Constants
+    public static final String USER_NAME = "MOVEit.username";
+    public static final String USER_PASSWORD = "MOVEit.password";
+    public static final String INVALID_USER_NAME = "MOVEit.invalidUsername";
+    public static final String INVALID_USER_PASSWORD = "MOVEit.invalidPassword";
+
+    // Login Page Constants
     public static final String LOGIN_USERNAME_FIELD_PATH = "loginPage.usernameField";
     public static final String LOGIN_PASSWORD_FIELD_PATH = "loginPage.passwordField";
     public static final String LOGIN_BUTTON_PATH = "loginPage.loginButton";
     public static final String LOGIN_SECURE_MESSAGE = "loginPage.secureMessage";
     public static final String LOGIN_ERROR_MESSAGE = "loginPage.errorMessage";
     public static final String LOGOUT_BUTTON = "homePage.logoutButton";
-    public static final String USER_NAME = "MOVEit.username";
-    public static final String USER_PASSWORD = "MOVEit.password";
-    public static final String INVALID_USER_NAME = "MOVEit.invalidUsername";
-    public static final String INVALID_USER_PASSWORD = "MOVEit.invalidPassword";
-    public static final String NEW_CONTACT_NAME = "John Doe";
-    public static final String NEW_CONTACT_MAIL = "john.doe@example.com";
-    public static final String CONTACT_CONFIRMATION_ERROR = "Contact addition confirmation message not found";
-    public static final String CONTACT_NOT_FOUND_ERROR = "John Doe is not visible on page";
+
+    // Package Management Constants
     public static final String PACKAGES_BUTTON_XPATH = "packagesPage.packagesButton";
     public static final String SEND_PACKAGE_BUTTON_XPATH = "packagesPage.sendPackageButton";
     public static final String RECIPIENT_FIELD_XPATH = "packagesPage.recipientField";
@@ -30,6 +34,8 @@ public class Constants {
     public static final String PACKAGE_RECIPIENT = "doychj@gmail.com";
     public static final String PACKAGE_SUBJECT = "test package for you";
     public static final String PACKAGE_BODY = "nice package!";
+
+    // Contact Management Constants
     public static final String CONTACTS_BUTTON = "contactsPage.contactsButton";
     public static final String ADD_CONTACT_BUTTON = "contactsPage.addContactButton";
     public static final String CONTACT_NAME = "contactsPage.contactName";
@@ -37,6 +43,12 @@ public class Constants {
     public static final String ADD_CONTACT_CONFIRMATION_BUTTON = "contactsPage.confirmAddContact";
     public static final String CONTACT_ADDED_CONFIRMATION_MESSAGE = "contactsPage.confirmMessage";
     public static final String RETURN_TO_ADDRESS_BOOK = "contactsPage.returnToAddressBook";
+    public static final String NEW_CONTACT_NAME = "John Doe";
+    public static final String NEW_CONTACT_MAIL = "john.doe@example.com";
+    public static final String CONTACT_CONFIRMATION_ERROR = "Contact addition confirmation message not found";
+    public static final String CONTACT_NOT_FOUND_ERROR = "John Doe is not visible on page";
+
+    // Home Folder Constants
     public static final String HOME_FOLDER_BUTTON = "homeFolder.homeFolderButton";
     public static final String UPLOAD_FILES_BUTTON = "homeFolder.uploadFilesButton";
     public static final String UPLOAD_BUTTON = "homeFolder.uploadButton";
@@ -44,16 +56,9 @@ public class Constants {
     public static final String DOWNLOAD_BUTTON = "homeFolder.downloadButton";
     public static final String FILE_NOT_UPLOADED_MESSAGE = "File not uploaded: %s";
     public static final String VALIDATION_MESSAGE_NOT_FOUND = "Validation message not found.";
-    public static final String TESTDATA_PATH = "src/test/resources/testdata/";
-    public static final String FILE_SELECTOR = "file-selector-input";
-    public static final String FILE_SPAN = "//span[contains(.,'%s')]";
-    public static final String DELETE_BUTTON = "//span[contains(.,'Delete')]";
-    public static final String DELETE_YES_BUTTON = "//span[contains(.,'Yes')]";
     public static final String VALIDATION_MESSAGE = "homeFolder.validationMessage";
-    public static final long UPLOAD_COMPLETION_WAIT_TIME = 10000;
-    public static final String URL_ERROR_MESSAGE = "Landed URL is not as expected. Actual URL: ";
-    public static final String EXPECTED_URL_MESSAGE = "'. Expected URL: '";
-    public static final String FILE_NAME = "surprise.docx";
+
+    // Dynamic XPath Constants
     public static final String PACKAGE_TITLE_XPATH = "//div[@title='%s'][contains(.,'%s')]";
     public static final String PACKAGE_SENT_CONFIRMATION_XPATH = "//div[contains(@class,'statuscontent')]";
     public static final String FILE_SELECTOR_INPUT_CLASS = "file-selector-input";
@@ -63,11 +68,22 @@ public class Constants {
     public static final String RECALL_BUTTON_XPATH = "//span[contains(.,'Recall')]";
     public static final String CONFIRM_RECALL_XPATH = "//span[contains(.,'Confirm Recall of Package')]";
     public static final String CONFIRM_YES_XPATH = "//span[contains(.,'Yes')]";
+    public static final String FILE_SPAN = "//span[contains(.,'%s')]";
+    public static final String DELETE_BUTTON = "//span[contains(.,'Delete')]";
+    public static final String DELETE_YES_BUTTON = "//span[contains(.,'Yes')]";
+    public static final String SEARCH_FIELD_XPATH = "//input[contains(@placeholder,'Search')]";
+    public static final String CONTACT_VISIBILITY_XPATH = "//td[@class='textsmall'][contains(.,'%s')]";
+
+    // Message Constants
     public static final String UPLOAD_NOT_VISIBLE_MSG = "Uploaded package %s is not visible.";
     public static final String PACKAGE_SENT_CONFIRMATION_NOT_FOUND_MSG = "Package sent confirmation message not found.";
     public static final String RECALL_STATUS_NOT_FOUND_MSG = "Recall status message not found.";
-    public static final String PACKAGE_BODY_SUBJECT = "test package for you";
-    public static final String SEARCH_FIELD_XPATH = "//input[contains(@placeholder,'Search')]";
-    public static final String CONTACT_VISIBILITY_XPATH = "//td[@class='textsmall'][contains(.,'%s')]";
-}
+    public static final String URL_ERROR_MESSAGE = "Landed URL is not as expected. Actual URL: ";
+    public static final String EXPECTED_URL_MESSAGE = "'. Expected URL: '";
 
+    // Miscellaneous Constants
+    public static final String FILE_NAME = "surprise.docx";
+    public static final String PACKAGE_BODY_SUBJECT = "test package for you";
+    public static final long UPLOAD_COMPLETION_WAIT_TIME = 10000;
+    public static final String FILE_SELECTOR = "file-selector-input";
+}
