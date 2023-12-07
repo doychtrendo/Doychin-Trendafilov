6f813b40ed674fca7a8351ccf365793a80033025
Doychin <doych_trendo@yahoo.com>
2023-12-07 18:27:14 +0200

    Add more options for different browser types

--- MOVEitTestServer/src/main/java/com/moveit/testframework/CustomWebDriverManager.java
+++ MOVEitTestServer/src/main/java/com/moveit/testframework/CustomWebDriverManager.java
@@ -1,18 +1,39 @@
 package com.moveit.testframework;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class CustomWebDriverManager {
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless");
+            String browserType = System.getProperty("browser", "firefox");
 
-            driver = new FirefoxDriver(options);
+            switch (browserType.toLowerCase()) {
+                case "firefox":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments("--headless");
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless");
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+                case "safari":
+                    SafariOptions safariOptions = new SafariOptions();
+                    driver = new SafariDriver(safariOptions);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Browser type not supported: " + browserType);
+            }
+
             driver.manage().window().maximize();
         }
         return driver;
